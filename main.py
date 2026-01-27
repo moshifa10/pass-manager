58997efe90f5847285ad25dcb59c3026c6e3a3a4
Njabulo Moshifa <moshifanjabulo@gmail.com>
2026-01-27 23:24:59 +0200

    added the ui setup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 BACKGROUND = '#816f6f'
 PATH = '/C:/Users/bumos/Documents'
+FONT_NAME = "Courier"
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
@@ -9,12 +10,27 @@ PATH = '/C:/Users/bumos/Documents'
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
+window.minsize(height=400, width=500)
 window.title("Password Manager")
-window.config(bg=BACKGROUND,padx=70, pady=70)
+window.config(bg=BACKGROUND, padx=50, pady=50)
 
+# canvas
 canvas = Canvas(width=200, height=224, bg=BACKGROUND, highlightthickness=0)
 photo = PhotoImage(file="./logo.png")
 canvas.create_image(100, 112, image=photo)
 canvas.grid(column=1, row=0)
+
+# ----------------------------------------------------------------------------------
+# ---------------- label Website ----------------
+website = Label(text="Website:", font=(FONT_NAME,11, "normal"), bg=BACKGROUND)
+website.grid(row=1, column=0)
+
+# ------------- Website input --------------
+website_input = Entry(width=20, font=(FONT_NAME, 11, "normal"))
+website_input.grid(row=1, column= 1)
+# ----------------------------------------------------------------------------------
+
+
+
 
 window.mainloop()
